7d5d8fe7b74dc436cde9dffe4b1e50ac2b2b561a
Maxim <47028566+Naymoll@users.noreply.github.com>
2020-11-12 19:43:23 +0700

    Tab fixes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 double my_pow(double value, int pow)
 {
-   double result = 1;
-	while (pow) 
+    double result = 1;
+    while (pow) 
     {
-		if (pow % 2 == 1)
+        if (pow % 2 == 1)
             result *= value;
 		
         value *= value;
-		pow /= 2;
-	}
+        pow /= 2;
+    }
 
-	return result;
+    return result;
 }
