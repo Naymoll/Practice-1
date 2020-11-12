99719bb7a48c655a72912205921146452995e448
Maxim <47028566+Naymoll@users.noreply.github.com>
2020-11-12 19:38:15 +0700

    Tab fixes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 double fast_pow(double value, unsigned int pow)
 {
    double result = 1;
-	while (pow) 
-    {
-		if (pow % 2 == 1)
-            result *= value;
+   while (pow) 
+   {
+       if (pow % 2 == 1)
+           result *= value;
 		
-        value *= value;
-		pow /= 2;
-	}
-
-	return result;
+       value *= value;
+       pow /= 2;
+   }
+   return result;
 }
